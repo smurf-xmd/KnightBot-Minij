aaef52aae68c8113178b03f4f3f4cd638cff2a45
SMURF-XMD <mythicfusion65@gmail.com>
2026-06-14 23:07:05 +0300

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
     // Bot Owner Configuration
-    ownerNumber: ['254756992340','254788409105'], // Add your number without + or spaces (e.g., 919876543210)
+    ownerNumber: ['25475692340','254788409105'], // Add your number without + or spaces (e.g., 919876543210)
     ownerName: ['Knight Bot Mini', 'Professor'], // Owner names corresponding to ownerNumber array
     
     // Bot Configuration
